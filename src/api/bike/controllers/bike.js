41c38a54f1e5e6f206b1e9eca6826f347f34877d
Romanize <robert.g.martinez.a@gmail.com>
2022-02-26 16:57:03 -0300

    feat: available bikes query

--- src/api/bike/controllers/bike.js
+++ src/api/bike/controllers/bike.js
@@ -1,9 +1,56 @@
-'use strict';
+"use strict";
 
 /**
  *  bike controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::bike.bike');
+module.exports = createCoreController("api::bike.bike", ({ strapi }) => ({
+  available: async (ctx) => {
+    const { query } = ctx.request;
+    console.log(query);
+
+    const response = await strapi.db.query("api::bike.bike").findMany({
+      where: {
+        $not: {
+          rentals: {
+            $or: [
+              {
+                $and: [
+                  {
+                    startDate: {
+                      $gte: query.startDate,
+                    },
+                  },
+                  {
+                    endDate: {
+                      $lte: query.endDate,
+                    },
+                  },
+                ],
+              },
+              {
+                $or: [
+                  {
+                    startDate: {
+                      $between: [query.startDate, query.endDate],
+                    },
+                  },
+                  {
+                    endDate: {
+                      $between: [query.startDate, query.endDate],
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      },
+      populate: ["rentals"],
+    });
+
+    ctx.body = response;
+  },
+}));
