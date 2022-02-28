11a838fd895339bd3b761830bb681288cb44cb50
Romanize <robert.g.martinez.a@gmail.com>
2022-02-28 16:54:59 -0300

    update endpoint

--- src/api/bike/controllers/bike.js
+++ src/api/bike/controllers/bike.js
@@ -10,35 +10,30 @@ module.exports = createCoreController("api::bike.bike", ({ strapi }) => ({
   available: async (ctx) => {
     const { query } = ctx.request;
     
-    if (!query.location || !query.startDate || !query.endDate) {
-      return ctx.badRequest('location, startDate and endDate are required')
+    if (!query.startDate || !query.endDate) {
+      return ctx.badRequest('startDate and endDate are required')
     }
 
     const response = await strapi.db.query("api::bike.bike").findMany({
       where: {
-        location: {
-          name: {
-            $eq: query.location,
-          }
-        },
         $not: {
           rentals: {
             $or: [
               {
                 startDate: {
-                  $between: [query.startDate, query.endDate],
-                },
+                  $between: [query.startDate,query.endDate]
+                }
               },
               {
                 endDate: {
-                  $between: [query.startDate, query.endDate],
-                },
-              },
-            ],
+                  $between: [query.startDate, query.endDate]
+                }
+              }
+            ]
           },
         },
       },
-      populate: ["location", "color", "pictures"],
+      populate: ['rentals', 'color', 'location', 'rates'],
     });
 
     ctx.body = response;
